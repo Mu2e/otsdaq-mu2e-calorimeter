3b30935a908248a0d8ff68df277bb018db07dc4c
Mu2e <mu2eshift@mu2e-cfo-01.fnal.gov>
2026-03-17 17:55:28 -0500

    Added FEE voltage and current in slow control

--- boardConfig/slowControl/slow_control_summary.sh
+++ boardConfig/slowControl/slow_control_summary.sh
@@ -5,19 +5,28 @@ DIR="./"  # puoi cambiarlo in un path assoluto
 
 # Nome del file di output
 SUMMARY_FILE="summary.log"
+SUMMARYVI_FILE="summaryVI.log"
 rightnow=$(date +'%Y%m%d_%H%M')
 SUMMARY_DATE="summary_${rightnow}.log"
+SUMMARYVI_DATE="summaryVI_${rightnow}.log"
 
 # Cancella o crea file riepilogo
 > "$SUMMARY_FILE"
 
 # Trova tutti i file slowControl*.log e li ordina numericamente
-for f in $(ls "${DIR}"/slowControl*.log 2>/dev/null | sort -V); do
+for f in $(ls "${DIR}"/slowControl[0-9]*.log 2>/dev/null | sort -V); do
     # Estrai l'ultima riga
     last_line=$(tail -n 1 "$f")
     echo "$last_line" >> "$SUMMARY_FILE"
 done
 
-cp $SUMMARY_FILE $SUMMARY_DATE
+for f in $(ls "${DIR}"/slowControlVI[0-9]*.log 2>/dev/null | sort -V); do
+    # Estrai l'ultima riga
+    last_line=$(tail -n 1 "$f")
+    echo "$last_line" >> "$SUMMARYVI_FILE"
+done
 
-echo "Creato riepilogo: $SUMMARY_DATE"
+cp $SUMMARY_FILE $SUMMARY_DATE
+cp $SUMMARYVI_FILE $SUMMARYVI_DATE
+
+echo "Creato riepilogo: $SUMMARY_DATE, $SUMMARYVI_DATE"
