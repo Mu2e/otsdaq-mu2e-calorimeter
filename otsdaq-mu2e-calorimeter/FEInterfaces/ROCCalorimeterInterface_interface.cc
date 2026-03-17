01084bb6e2c2a00ba19b9ba49e540bfeb0284a85
Mu2e <mu2eshift@mu2e-mgr-01.fnal.gov>
2026-03-17 11:11:46 -0500

    clang

--- otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterInterface_interface.cc
+++ otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterInterface_interface.cc
@@ -2278,7 +2278,7 @@ TableVersion ROCCalorimeterInterface::CreateGlobalROCTable(std::ostream& os, boo
 
 						if(rocCache[roc.first][name] == value) {
 							__FE_COUT__ << "Board ID already correct for ROC " << roc.first << " on DTC " << targetDTC << ": cached value = " << rocCache[roc.first][name] << ", read value = " << value
-							                                                << __E__;
+							            << __E__;
 							os << "* Board ID already correct for " << roc.first << ": " << value << __E__;
 							continue;
 						}
